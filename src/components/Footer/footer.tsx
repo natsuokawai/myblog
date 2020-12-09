166342a73cf6316b55dc1fda3b9ae4f25280ea5b
natsuokawai <kawai.natsuo@gmail.com>
2020-12-09 21:02:53 +0900

    Modify footer

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,15 +1,8 @@
 import React from "react"
-import { Link } from "gatsby"
-import SocialLinks from "../SocialLinks/sociallinks"
-
 import "./footer.scss"
 
 const Footer = props => (
 	<footer className={`Footer ${props.className}`}>
-		<Link to="/" className="Footer__title">
-			{props.siteTitle}
-		</Link>
-		<SocialLinks />
 		<span className="Footer__legal">
 			© {new Date().getFullYear()} {props.siteAuthor}. All rights reserved.
 		</span>
